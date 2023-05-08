c7c7c6651d9f9e97fa15467973ad339dbf5e6031
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-08 12:22:13 +0200

    💩Event is send to REST Endpoint.

--- src/main/java/com/cevher/keycloak/CustomEventListenerProvider.java
+++ src/main/java/com/cevher/keycloak/CustomEventListenerProvider.java
@@ -1,5 +1,8 @@
 package com.cevher.keycloak;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
@@ -12,6 +15,7 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.models.RealmProvider;
 import org.keycloak.models.UserModel;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static com.cevher.keycloak.Client.postService;
@@ -24,7 +28,21 @@ public class CustomEventListenerProvider
     public void onEvent(Event event) {
         if(EventType.REGISTER.equals(event.getType())) {
             System.out.println("User Registered: " + event.getUserId());
-            //postService(event.toString());
+
+            String json;
+            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            try {
+                json = ow.writeValueAsString(event);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+
+            try {
+                postService(json);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -32,7 +50,21 @@ public class CustomEventListenerProvider
     public void onEvent(AdminEvent event, boolean includeRepresentation) {
         if(OperationType.CREATE.equals(event.getOperationType()) && ResourceType.USER.equals(event.getResourceType())) {
             System.out.println("User Created: " + event.getResourcePath());
-            //postService(event.toString());
+
+            String json;
+            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            try {
+                json = ow.writeValueAsString(event);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+
+            try {
+                postService(json);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
