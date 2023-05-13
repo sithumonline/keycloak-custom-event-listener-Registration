f66d7bbcc659475389b91dabfe2e8c43ce2f2e02
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-13 22:40:33 +0200

    🐛 User Self Registration Event Works now

--- src/main/java/com/cevher/keycloak/CustomEventListenerProvider.java
+++ src/main/java/com/cevher/keycloak/CustomEventListenerProvider.java
@@ -3,7 +3,6 @@ package com.cevher.keycloak;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
@@ -11,24 +10,23 @@ import org.keycloak.events.admin.AdminEvent;
 import org.keycloak.events.admin.OperationType;
 import org.keycloak.events.admin.ResourceType;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.RealmProvider;
-import org.keycloak.models.UserModel;
 
+import javax.inject.Inject;
 import java.io.IOException;
-import java.util.Map;
 
-import static com.cevher.keycloak.Client.postService;
+import static com.cevher.keycloak.Client.postServiceAdminEvent;
+import static com.cevher.keycloak.Client.postServiceUserEvent;
 
 public class CustomEventListenerProvider
         implements EventListenerProvider {
+
 
 
     //TODO: Fix this
     // AdminEvent is working correctly
     @Override
     public void onEvent(Event event) {
-        if(EventType.REGISTER.equals(event.getType())) {
+        if (EventType.REGISTER.equals(event.getType())) {
             System.out.println("User Registered with self registration : " + event.getUserId());
 
             String json;
@@ -38,10 +36,10 @@ public class CustomEventListenerProvider
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
-
-
             try {
-                postService(json);
+
+                // Post to *User* Service
+                postServiceUserEvent(json);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -51,7 +49,7 @@ public class CustomEventListenerProvider
 
     @Override
     public void onEvent(AdminEvent event, boolean includeRepresentation) {
-        if(OperationType.CREATE.equals(event.getOperationType()) && ResourceType.USER.equals(event.getResourceType())) {
+        if (OperationType.CREATE.equals(event.getOperationType()) && ResourceType.USER.equals(event.getResourceType())) {
             System.out.println("User Created through admin: " + event.getResourcePath());
 
             String json;
@@ -61,10 +59,10 @@ public class CustomEventListenerProvider
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
-
-
             try {
-                postService(json);
+
+                // Post to *Admin* Service
+                postServiceAdminEvent(json);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
